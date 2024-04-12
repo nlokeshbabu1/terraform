09c1e91f473f5ee7370a81dc0b1853f09b720b03
nlokeshbabu1 <138300460+nlokeshbabu1@users.noreply.github.com>
2024-04-12 15:22:36 +0530

    Add files via upload
    This is how to create aks using terraform

---
+++ azure/aks_main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "my_cluster" {
+    name = "azure_my"
+    location = "West US 2"
+
+    tags = {
+      environment = "demo"
+    }
+
+  
+}
+
+resource "azurerm_kubernetes_cluster" "my_cluster" {
+    name = "azure-aks"
+    location = azurerm_resource_group.my_cluster.location
+    resource_group_name = azurerm_resource_group.my_cluster.name
+    dns_prefix = "azure-k8s"
+
+    default_node_pool {
+      name = "default"
+      node_count = 2
+      vm_size = "Standard_D2_v2"
+      os_disk_size_gb = 30
+    }
+
+    service_principal {
+      client_id = var.appId
+      client_secret = var.password
+    }
+
+    role_based_access_control_enabled = true
+}
+  
