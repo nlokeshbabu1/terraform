a5bdb97b9b3c2ed7a58a725aac46bc50c27cba4b
nlokeshbabu1 <lokeshbabu.nalluri1111@gmail.com>
2025-01-30 10:33:42 +0000

    updated the iam-role.tf

--- aws/eks-modules/modules/iam-role.tf
+++ aws/eks-modules/modules/iam-role.tf
@@ -1,5 +1,5 @@
 
-#This is a role policy attachment where aws interactive with the resource on behalf of the user
+#This is a role  aws where interactive with the resource on behalf of the user
 resource "aws_iam_role" "eks-iam-role" {
   name = "eks-iam-role"
   assume_role_policy = <<EOF
@@ -21,13 +21,13 @@ EOF
 }
 
 
-#This policy is used to attach the policy to the eks_cluster_policy
+#This policy is used to attach the role to the eks_iam-role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role = aws_iam_role.eks-iam-role.name
   
 }
-#This is a role policy attachment where aws interactive with the resource on behalf of the user
+#This is a role where aws interactive with the resource on behalf of the user
 resource "aws_iam_role" "eks_node_policy" {
     name = "eks_node_policy"
     assume_role_policy = <<EOF
@@ -48,25 +48,31 @@ resource "aws_iam_role" "eks_node_policy" {
 EOF
 }
 
-#This policy is used to attach the policy to the eks_worker_node_policy
+#This policy is used to attach to the role  eks_node_policy
+#This policy allows Amazon EKS worker nodes to connect to Amazon EKS Clusters.
 resource "aws_iam_role_policy_attachment" "eks_node_policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
     role = aws_iam_role.eks_node_policy.name
 }
 
+#This policy is used to attach to the role  eks_node_policy
+#This policy allows Amazon Elastic Container Service for Kubernetes to create and manage the necessary resources to operate EKS Clusters.
 resource "aws_iam_role_policy_attachment" "eks_service_policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
     role = aws_iam_role.eks_node_policy.name
   
 }
 
-
+#This policy is used to attach to the role  eks_node_policy
 #This policy is used for managing the network in worker nodes
+#These policies can be used to control incoming and outgoing traffic to and from the worker nodes.
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
     role = aws_iam_role.eks_node_policy.name
   
 }
+
+#This policy is used to attach to the role  eks_node_policy
 #This policy is used for accessing ECR
 resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
